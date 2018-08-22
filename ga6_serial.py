f1f347644aa1261c0be3d805f57aa3d57a08da4d
Jonathan <jonathan.nuance@outlook.com>
2018-08-22 20:40:08 +0800

    Complete ga6_serial dial and message function.

--- ga6_serial.py
+++ ga6_serial.py
@@ -4,8 +4,6 @@ import threading
 
 
 class GA6Serial(serial.Serial):
-    # def listen(self):
-
     def check_serial(self):
         self.write(b'AT\r\n')
 
@@ -25,8 +23,25 @@ class GA6Serial(serial.Serial):
         num = str(num)
         self.write(f'AT+CMGR={num}\r\n'.encode())
 
-    def dial(self, number):
-        self.write(f'ATD{number}\r\n'.encode())
+    def delete_msg(self, num):
+        num = str(num)
+        self.write(f'AT+CMGD={num}\r\n'.encode())
+
+    def delete_read_msg(self):
+        self.write(f'AT+CMGD=1,1\r\n'.encode())
+
+    def delete_read_sent_msg(self):
+        self.write(f'AT+CMGD=1,2\r\n'.encode())
+
+    def delete_read_sent_unsend_msg(self):
+        self.write(f'AT+CMGD=1,3\r\n'.encode())
+
+    def delete_all_msg(self):
+        self.write(f'AT+CMGD=1,4\r\n'.encode())
+
+    def dial(self, num):
+        num = str(num)
+        self.write(f'ATD{num}\r\n'.encode())
 
     def pick_up(self):
         self.write(b'ATA\r\n')
@@ -58,18 +73,23 @@ class GA6Serial(serial.Serial):
     def set_charset_gsm(self):
         self.write(b'AT+CSCS="GSM"\r\n')
 
-    def set_msg_target(self, number):
-        self.write(f'AT+CMGS="{number}"\r\n'.encode())
+    def set_msg_target(self, num):
+        self.write(f'AT+CMGS="{num}"\r\n'.encode())
+
+    def set_msg_length(self, num):
+        num = str(num)
+        self.write(f'AT+CMGS={num}\r\n'.encode())
+
+    # SM,ME,MT
+    def set_msg_storage(self, mem1, mem2, mem3):
+        self.write(f'AT+CPMS="{mem1}","{mem2}","{mem3}"\r\n'.encode())
 
     def set_msg_content(self, content):
+        content = str(content)
         self.write(content.encode())
 
     def send_msg(self):
         self.write(b'\x1A')
 
 
-ser = GA6Serial('/dev/ttyAMA0', 115200, timeout=0.5)
-# ser = GA6Serial(PORT, BAUD_RATE, timeout=TIMEOUT)
-
-ser.write(b'AT\r\n')
-ser.readline()
+ser = GA6Serial(PORT, BAUD_RATE, timeout=TIMEOUT)
