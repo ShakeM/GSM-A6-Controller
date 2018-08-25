88df094abede78550b1476fe8fff5f5b5a19a03b
jonathan <jonathan.nuance@outlook.com>
2018-08-25 10:03:14 +0800

    取消多重继承，将Console嵌入EasyA6

--- easy_a6.py
+++ easy_a6.py
@@ -2,18 +2,18 @@ from config import *
 from core import GA6Core
 from pdu_converter import PDUConverter
 from console import Console
+import time
 
 
 class EasyA6(GA6Core):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.record = []
         self.reader = Console(self)
         self.reader.start()
 
-        self.check_signal()
-        self.display_caller_id()
+        self.wait(self.check_signal())
+        # self.display_caller_id()
         self.smsc = SMSC
 
     def send(self, recevier, content):
@@ -23,5 +23,27 @@ class EasyA6(GA6Core):
         self.set_msg_content(content.encode())
         self.send_msg()
 
+    def wait(self, foo, timeout=RESPONSE_TIMEOUT):
+        result = foo()
 
-ser = EasyA6(PORT, BAUD_RATE, timeout=TIMEOUT)
+        pass_time = 0
+        sent = False
+        while True:
+            if result in self.reader.lines and sent == False:
+                print(result)
+                sent = True
+            elif 'OK\r\n' in result and sent == True:
+                print('Done')
+                break
+            elif [e for e in result if 'ERROR:' in e]:
+                print([e for e in result if 'ERROR:' in e])
+                break
+            elif pass_time > timeout:
+                print('Timeout')
+                break
+
+            pass_time += 0.2
+            time.sleep(0.2)
+
+
+ser = EasyA6(PORT, BAUD_RATE, timeout=READ_TIMEOUT)
