ef7ec98f2c06d9bc702bdfb0e6fd87359e800aa2
jonathan <jonathan.nuance@outlook.com>
2018-08-25 09:13:33 +0800

    core 为每个函数添加自我返回值

--- core.py
+++ core.py
@@ -3,103 +3,169 @@ import serial
 
 class GA6Core(serial.Serial):
     def check_signal(self):
-        self.write(b'AT\r\n')
+        instruction = b'AT\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_sim_status(self):
-        self.write(b'AT+CPIN?\r\n')
+        instruction = b'AT+CPIN?\r\n'
+        instruction = instruction
+        self.write(instruction)
+        return instruction.decode()
 
     def get_registration(self):
-        self.write(b'AT+CREG?\r\n')
+        instruction = b'AT+CREG?\r\n'
+        instruction = instruction
+        self.write(instruction)
+        return instruction.decode()
 
     def get_imei(self):
-        self.write(b'AT+GSN\r\n')
+        instruction = b'AT+GSN\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_msg_storage(self):
-        self.write(b'AT+CPMS?\r\n')
+        instruction = b'AT+CPMS?\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_msg(self, num):
         num = str(num)
-        self.write(f'AT+CMGR={num}\r\n'.encode())
+        instruction = f'AT+CMGR={num}\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def get_unread_msg(self):
-        self.write(b'AT+CMGL=0')
+        instruction = b'AT+CMGL=0'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_read_msg(self):
-        self.write(b'AT+CMGL=1')
+        instruction = b'AT+CMGL=1'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_draft_msg(self):
-        self.write(b'AT+CMGL=2')
+        instruction = b'AT+CMGL=2'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_sent_msg(self):
-        self.write(b'AT+CMGL=3')
+        instruction = b'AT+CMGL=3'
+        self.write(instruction)
+        return instruction.decode()
 
     def get_all_msg(self):
-        self.write(b'AT+CMGL=4')
+        instruction = b'AT+CMGL=4'
+        self.write(instruction)
+        return instruction.decode()
 
     def delete_msg(self, num):
         num = str(num)
-        self.write(f'AT+CMGD={num}\r\n'.encode())
+        instruction = f'AT+CMGD={num}\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def delete_read_msg(self):
-        self.write(f'AT+CMGD=1,1\r\n'.encode())
+        instruction = f'AT+CMGD=1,1\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def delete_read_sent_msg(self):
-        self.write(f'AT+CMGD=1,2\r\n'.encode())
+        instruction = f'AT+CMGD=1,2\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def delete_read_sent_unsend_msg(self):
-        self.write(f'AT+CMGD=1,3\r\n'.encode())
+        instruction = f'AT+CMGD=1,3\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def delete_all_msg(self):
-        self.write(f'AT+CMGD=1,4\r\n'.encode())
+        instruction = f'AT+CMGD=1,4\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def dial(self, num):
         num = str(num)
-        self.write(f'ATD{num}\r\n'.encode())
+        instruction = f'ATD{num}\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def pick_up(self):
-        self.write(b'ATA\r\n')
+        instruction = b'ATA\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def hang_up(self):
-        self.write(b'ATH\r\n')
+        instruction = b'ATH\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def redial(self):
-        self.write(b'AT+DLST\r\n')
+        instruction = b'AT+DLST\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def display_caller_id(self):
-        self.write(b'AT+CLIP=1\r\n')
+        instruction = b'AT+CLIP=1\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def conceal_caller_id(self):
-        self.write(b'AT+CLIP=0\r\n')
+        instruction = b'AT+CLIP=0\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def shift_headset(self):
-        self.write(b'AT+SNFS=1\r\n')
+        instruction = b'AT+SNFS=1\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def shift_horn(self):
-        self.write(b'AT+SNFS=0\r\n')
+        instruction = b'AT+SNFS=0\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def set_mode_text(self):
-        self.write(b'AT+CMGF=1\r\n')
+        instruction = b'AT+CMGF=1\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def set_mode_pdu(self):
-        self.write(b'AT+CMGF=0\r\n')
+        instruction = b'AT+CMGF=0\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def set_charset_gsm(self):
-        self.write(b'AT+CSCS="GSM"\r\n')
+        instruction = b'AT+CSCS="GSM"\r\n'
+        self.write(instruction)
+        return instruction.decode()
 
     def set_msg_target(self, num):
-        self.write(f'AT+CMGS="{num}"\r\n'.encode())
+        instruction = f'AT+CMGS="{num}"\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def set_msg_len(self, num):
         num = str(num)
-        self.write(f'AT+CMGS={num}\r\n'.encode())
+        instruction = f'AT+CMGS={num}\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     # SM,ME,MT
     def set_msg_storage(self, mem1, mem2, mem3):
-        self.write(f'AT+CPMS="{mem1}","{mem2}","{mem3}"\r\n'.encode())
+        instruction = f'AT+CPMS="{mem1}","{mem2}","{mem3}"\r\n'.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def set_msg_content(self, content):
         content = str(content)
-        self.write(content.encode())
+        instruction = content.encode()
+        self.write(instruction)
+        return instruction.decode()
 
     def send_msg(self):
-        self.write(b'\x1A')
+        instruction = b'\x1A'
+        self.write(instruction)
+        return instruction.decode()
